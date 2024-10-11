e2f9e5ebb0b807caa402fba677ec1df27a8cb1cf
dawangran <605547565@qq.com>
2024-10-11 21:53:41 +0800

    add pip

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
-from setuptools import setup 
-from setuptools import find_packages
+# -*- coding: utf-8 -*-
 
-setup(name='BCscannerBWA',
-      version='0.0.1',
-      description='Barcode split for Cyclone',
-      author='dawn',
-      author_email='605547565@qq.com',
-      requires= ['pandas','Levenshtein','numpy','anndata','samtools','pysam'], # 定义依赖哪些模块
-      packages=find_packages(),  # 系统自动从当前目录开始找包
-      license="apache 3.0",
-      python_requires='>=3.6'
-      )
+
+
+import setuptools
+ 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    name="BCscannerBWA", 
+    version="1.0.0",
+    author="Dawn",
+    author_email="605547565@qq.com",
+    description='Barcode split for Cyclone',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dawangran/BCscannerBWA",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8'
+    
+)
